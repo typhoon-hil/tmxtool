8ccc269c6ec8187654843dbc0ec4499d64856130
Milan Djurisic <milan.djurisic@typhoon-hil.com>
2020-02-06 13:28:37 +0100

    Document printing_utilities

--- printing_utilities.py
+++ printing_utilities.py
@@ -1,6 +1,16 @@
 
 
-# -- Use instead of printing, if later printing messages needs to be changed
 def display_message(*messages):
+    """
+    Prints a message for the user on the console.
+
+    Use instead of printing, if later printing messages needs to be changed
+
+    Arguments:
+        messages: To be printed to the user
+
+    Returns:
+        None
+    """
     for message in messages:
         print(message)
