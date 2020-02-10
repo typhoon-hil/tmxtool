32fa5afa188439ad3edd1f895243bf4b34845e80
Milan Djurisic <milan.djurisic@typhoon-hil.com>
2020-02-10 18:26:05 +0100

    Add gui items
    Started work on tmx-to-vtt-panel to generate vtt files from tmx files

---
+++ tmx_gui/tmx_to_vtt_panel.py
@@ -0,0 +1,91 @@
+from tkinter import Tk, Label, Button, Radiobutton, filedialog
+import tkinter
+
+SELECTION_SINGLE_FILE = 'SINGLE_FILE'
+SELECTION_MULTIPLE_FILES = 'MULTIPLE_FILES'
+
+
+class TmxToVttPanel(tkinter.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        super(TmxToVttPanel, self).__init__(parent, *args, **kwargs)
+        self.parent = parent
+
+        # -- Configure invisible items
+        self.string_var_selected_radio = tkinter.StringVar()
+        self.string_var_single_file = tkinter.\
+            StringVar(self.parent, value='Path to tmx file ...')
+        self.string_var_multiple_files = tkinter.\
+            StringVar(self.parent, value='Path to directory ...')
+
+        # -- Configure row 1
+        row = 0
+        column = 0
+        self.radio_single_file = Radiobutton(self.parent,
+                                             text='Single File',
+                                             indicator=1,
+                                             value=SELECTION_SINGLE_FILE,
+                                             variable=self.
+                                             string_var_selected_radio,
+                                             command=self.radio_selection)
+        self.radio_single_file.grid(row=row, column=column, sticky=tkinter.W)
+        column += 1
+        self.textbox_single_file = tkinter. \
+            Entry(self.parent, textvariable=self.string_var_single_file)
+        self.textbox_single_file.grid(row=row, column=column)
+        column += 1
+        self.button_browse_single_file = \
+            tkinter.Button(self.parent,
+                           text="Browse",
+                           command=self.browse_button_file_clicked)
+        self.button_browse_single_file.grid(row=row, column=column)
+
+        # -- Configure row 2
+        row += 1
+        column = 0
+        self.radio_multiple_files = Radiobutton(self.parent,
+                                                text='Multiple Files',
+                                                indicator=1,
+                                                value=SELECTION_MULTIPLE_FILES,
+                                                variable=
+                                                self.string_var_selected_radio,
+                                                command=self.radio_selection)
+        self.radio_single_file.select()
+        self.radio_multiple_files.grid(row=row, column=column, sticky=tkinter.W)
+        column += 1
+        self.textbox_multiple_files = tkinter.\
+            Entry(self.parent, textvariable=self.string_var_multiple_files,
+                  state=tkinter.DISABLED)
+        self.textbox_multiple_files.grid(row=row, column=column)
+        column += 1
+        self.button_browse_multiple_files = \
+            tkinter.Button(self.parent,
+                           text="Browse",
+                           command=self.browse_button_directory_clicked,
+                           state=tkinter.DISABLED)
+        self.button_browse_multiple_files.grid(row=row, column=column)
+
+    def radio_selection(self):
+        selection = self.string_var_selected_radio.get()
+
+        if selection == SELECTION_MULTIPLE_FILES:
+            self.button_browse_multiple_files.configure(state=tkinter.NORMAL)
+            self.button_browse_single_file.configure(state=tkinter.DISABLED)
+            self.textbox_multiple_files.configure(state=tkinter.NORMAL)
+            self.textbox_single_file.configure(state=tkinter.DISABLED)
+        else:
+            self.button_browse_multiple_files.configure(state=tkinter.DISABLED)
+            self.button_browse_single_file.configure(state=tkinter.NORMAL)
+            self.textbox_multiple_files.configure(state=tkinter.DISABLED)
+            self.textbox_single_file.configure(state=tkinter.NORMAL)
+
+    def browse_button_directory_clicked(self):
+        dirname = filedialog.askdirectory(initialdir="/",
+                                          title="Select a directory")
+        print(dirname)
+
+    def browse_button_file_clicked(self):
+        filename = filedialog.askopenfilename(initialdir="/",
+                                              title="Select .tmx File",
+                                              filetype=(("tmx files", "*.tmx"),
+                                                        ("all files", "*.*")))
+        print(filename)
