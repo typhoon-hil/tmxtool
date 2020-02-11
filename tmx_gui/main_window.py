1ed25bca21248243c366b5895445d7fed5eff80d
Milan Djurisic <milan.djurisic@typhoon-hil.com>
2020-02-11 10:56:32 +0100

    Update main window

--- tmx_gui/main_window.py
+++ tmx_gui/main_window.py
@@ -1,27 +1,71 @@
-from tkinter import Tk, Label, Button
+import tkinter
+from tkinter import Tk, Label, Button, Frame
 
-from tmx_gui.tmx_to_vtt_panel import TmxToVttPanel
+import CONSTANTS as const
 
 
-class MyFirstGUI:
-    def __init__(self, master):
+class MainWindow(Frame):
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
         self.master = master
-        master.title("A simple GUI")
+        # -- Shorthand for grid configurations - add to all grid configs
+        gc = {
+            'padx': 25,
+            'pady': 10,
+            'sticky': 'we'
+        }
 
-        self.label = Label(master, text="This is our first GUI!")
-        self.label.pack()
+        master.title("Linguist Support")
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
+        # -- Create buttons
+        self.button_generate_tmx = Button(self.master,
+                                          text="Generate .tmx",
+                                          command=lambda: self.open_window(
+                                              const.WINDOW_GENERATE_TMX))
+        self.button_generate_vtt = Button(self.master,
+                                          text="Generate .vtt",
+                                          command=lambda: self.open_window(
+                                              const.WINDOW_GENERATE_VTT))
+        self.button_about = Button(self.master,
+                                   text="About",
+                                   command=lambda: self.open_window(
+                                       const.WINDOW_ABOUT
+                                   ))
+        self.button_exit = Button(self.master,
+                                  text="Exit",
+                                  command=lambda: exit(0))
 
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
+        row = 0
+        column = 0
+        self.button_generate_tmx.grid(row=row, column=column, **gc)
+        self.master.grid_rowconfigure(row, weight=2)
+        row += 1
+        self.button_generate_vtt.grid(row=row, column=column, **gc)
+        self.master.grid_rowconfigure(row, weight=2)
+        row += 1
+        self.button_about.grid(row=row, column=column, **gc)
+        self.master.grid_rowconfigure(row, weight=2)
+        row += 1
+        self.button_exit.grid(row=row, column=column, **gc)
+        self.master.grid_rowconfigure(row, weight=2)
 
-    def greet(self):
-        print("Greetings!")
+        self.master.grid_columnconfigure(0, weight=2)
+        self.master.grid_rowconfigure(row, weight=2)
+
+    @staticmethod
+    def open_window(window_flag):
+        if window_flag == const.WINDOW_GENERATE_TMX:
+            # -- create generate tmx window
+            pass
+        elif window_flag == const.WINDOW_GENERATE_VTT:
+            # -- create generate vtt window
+            pass
 
 
 if __name__ == '__main__':
     root = Tk()
-    my_gui = TmxToVttPanel(root)
+    root.geometry("300x280")
+
+    my_gui = MainWindow(root, width=400)
+
     root.mainloop()
--- tmx_gui/tmx_to_vtt_panel.py
+++ tmx_gui/tmx_to_vtt_panel.py
@@ -6,43 +6,57 @@ SELECTION_MULTIPLE_FILES = 'MULTIPLE_FILES'
 
 
 class TmxToVttPanel(tkinter.Frame):
-    def __init__(self, parent, *args, **kwargs):
-        super(TmxToVttPanel, self).__init__(parent, *args, **kwargs)
-        self.parent = parent
+    def __init__(self, master, *args, **kwargs):
+        super(TmxToVttPanel, self).__init__(master, *args, **kwargs)
+        self.master = master
+
+        # -- Setup some decorations
+        grid_configurations = {'padx': 5,
+                               'pady': 5, }
 
         # -- Configure invisible items
         self.string_var_selected_radio = tkinter.StringVar()
         self.string_var_single_file = tkinter.\
-            StringVar(self.parent, value='Path to tmx file ...')
+            StringVar(self.master, value='Path to tmx file ...')
         self.string_var_multiple_files = tkinter.\
-            StringVar(self.parent, value='Path to directory ...')
+            StringVar(self.master, value='Path to directory ...')
 
-        # -- Configure row 1
+        # -- Configure items for labeling and showing help
+        
+
+        # -- Configure items for selecting single file
+        # -- Radio
         row = 0
         column = 0
-        self.radio_single_file = Radiobutton(self.parent,
+        self.radio_single_file = Radiobutton(self.master,
                                              text='Single File',
                                              indicator=1,
                                              value=SELECTION_SINGLE_FILE,
                                              variable=self.
                                              string_var_selected_radio,
                                              command=self.radio_selection)
-        self.radio_single_file.grid(row=row, column=column, sticky=tkinter.W)
+        self.radio_single_file.grid(row=row, column=column, sticky=tkinter.W,
+                                    **grid_configurations)
+        # -- Textbox
         column += 1
         self.textbox_single_file = tkinter. \
-            Entry(self.parent, textvariable=self.string_var_single_file)
-        self.textbox_single_file.grid(row=row, column=column)
+            Entry(self.master, textvariable=self.string_var_single_file, )
+        self.textbox_single_file.grid(row=row, column=column, sticky='we',
+                                      **grid_configurations)
+        # -- Button
         column += 1
         self.button_browse_single_file = \
-            tkinter.Button(self.parent,
+            tkinter.Button(self.master,
                            text="Browse",
                            command=self.browse_button_file_clicked)
-        self.button_browse_single_file.grid(row=row, column=column)
+        self.button_browse_single_file.grid(row=row, column=column,
+                                            **grid_configurations)
 
-        # -- Configure row 2
+        # -- Configure items for selecting directory
+        # -- Radio
         row += 1
         column = 0
-        self.radio_multiple_files = Radiobutton(self.parent,
+        self.radio_multiple_files = Radiobutton(self.master,
                                                 text='Multiple Files',
                                                 indicator=1,
                                                 value=SELECTION_MULTIPLE_FILES,
@@ -50,19 +64,39 @@ class TmxToVttPanel(tkinter.Frame):
                                                 self.string_var_selected_radio,
                                                 command=self.radio_selection)
         self.radio_single_file.select()
-        self.radio_multiple_files.grid(row=row, column=column, sticky=tkinter.W)
+        self.radio_multiple_files.grid(row=row, column=column, sticky=tkinter.W,
+                                       **grid_configurations)
+        # -- Textbox
         column += 1
         self.textbox_multiple_files = tkinter.\
-            Entry(self.parent, textvariable=self.string_var_multiple_files,
+            Entry(self.master, textvariable=self.string_var_multiple_files,
                   state=tkinter.DISABLED)
-        self.textbox_multiple_files.grid(row=row, column=column)
+        self.textbox_multiple_files.grid(row=row, column=column, sticky='we',
+                                         **grid_configurations)
+        # -- Button
         column += 1
         self.button_browse_multiple_files = \
-            tkinter.Button(self.parent,
+            tkinter.Button(self.master,
                            text="Browse",
                            command=self.browse_button_directory_clicked,
                            state=tkinter.DISABLED)
-        self.button_browse_multiple_files.grid(row=row, column=column)
+        self.button_browse_multiple_files.grid(row=row, column=column,
+                                               **grid_configurations)
+
+        #  -- Configure items for generating based on selection
+        # -- Configure generate action
+        row += 1
+        column = 0
+        self.button_generate = Button(self.master,
+                                      text="Generate .vtt",
+                                      command=self.generate_vtt)
+        self.button_generate.grid(row=row, column=column,
+                                  columnspan=3, **grid_configurations)
+
+        self.master.columnconfigure(1, weight=2)
+
+    def generate_vtt(self):
+        print("Generate vtt")
 
     def radio_selection(self):
         selection = self.string_var_selected_radio.get()
@@ -81,11 +115,11 @@ class TmxToVttPanel(tkinter.Frame):
     def browse_button_directory_clicked(self):
         dirname = filedialog.askdirectory(initialdir="/",
                                           title="Select a directory")
-        print(dirname)
+        self.string_var_multiple_files.set(dirname)
 
     def browse_button_file_clicked(self):
         filename = filedialog.askopenfilename(initialdir="/",
                                               title="Select .tmx File",
                                               filetype=(("tmx files", "*.tmx"),
                                                         ("all files", "*.*")))
-        print(filename)
+        self.string_var_single_file.set(filename)
